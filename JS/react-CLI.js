78d8658cdb92c7024b874b9a8e6a9396b3952b93
wanjiku <elidakirigo@gmail.com>
2020-08-27 23:39:31 +0300

    CLI

---
+++ JS/react-CLI.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------------------------------------
+ * CREATING REACT APP
+ * -------------------
+ * npm install -g create-react-app 
+ * 
+ * create-react-app  my-react-project
+ * 
+ * npm start / npm run build
+ * --------------------------------------------------
+ */
+
+
--- JS/webpack.js
+++ JS/webpack.js
@@ -6,6 +6,11 @@
  * npm install babel-core babel-loader babel-preset-env
  * babel-preset-react babel-preset-stage-o --save-dev
  * 
+ * MINIFYING / UGLYFYING WITH WEBPACK
+ * ----------------------------------
+ * npm install webpack --save-dev
+ * setting 'warnings' to false removes all console logs in the output. 
+ * 
  * ADDING REACT WITH WEBPACK
  * --------------------------
  * npm install react react-dom --save
@@ -31,7 +36,14 @@
                 presets: ['env', 'stage-0', 'react']
             }
         }]
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            warnings: false,
+            mangle: true
+        })    
+    ]
  }
 * --------------------------------------------
 */
